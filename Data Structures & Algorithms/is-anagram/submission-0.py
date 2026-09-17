f0e15e040fb20be859b87c10ab66411037f78ec5
atsi2004 <60413782+atsi2004@users.noreply.github.com>
2026-09-17 14:04:57 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_freq = {}
+        t_freq = {}
+
+        for char in s: # iterate through s string
+            if char in s_freq: # if character already in dict
+                s_freq[char] += 1 # add 1 to its freq
+            else:
+                s_freq[char] = 1 # otherwise set freq to 1
+        for char in t:
+            if char in t_freq:
+                t_freq[char] += 1
+            else:
+                t_freq[char] = 1
+    
+        if(t_freq == s_freq): # if the frequencies of both are the same
+            return True # strings are anagrams
+        else:
+            return False
+
